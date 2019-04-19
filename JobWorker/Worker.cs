1f9cea6297fac26378bbb8a64b58fad5254268ab
Danilo Novakovic <dakenzi97@gmail.com>
2019-04-19 11:16:51 +0200

    Created implementation of IWorker aswell as unit tests for it.

---
+++ JobWorker/Worker.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Common;
+
+namespace JobWorker
+{
+    public class Worker : IWorker
+    {
+        private string containerId;
+
+        /// <summary>
+        /// Starts the worker
+        /// </summary>
+        /// <param name="containerId">Id of container that called worker</param>
+        /// <exception cref="ArgumentException">ContainerId is null, empty or white space</exception>
+        public void Start(string containerId)
+        {
+            if (string.IsNullOrWhiteSpace(containerId))
+            {
+                throw new ArgumentException($"{nameof(containerId)} mustn't be null, empty or white space!");
+            }
+
+            this.containerId = containerId;
+            Console.WriteLine($"{containerId}: Worker started");
+        }
+
+        /// <summary>
+        /// Stops the worker
+        /// </summary>
+        /// <exception cref="InvalidOperationException">Stop has been called before Start</exception>
+        public void Stop()
+        {
+            if (this.containerId is null)
+            {
+                throw new InvalidOperationException($"Worker hasn't been started yet.");
+            }
+
+            Console.WriteLine($"{this.containerId}: Worker stopped");
+        }
+    }
+}
---
+++ JobWorkerTests/WorkerTests.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using JobWorker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobWorker.Tests
+{
+    [TestFixture]
+    public class WorkerTests
+    {
+        private Worker worker;
+
+        [SetUp]
+        public void SetUp()
+        {
+            worker = new Worker();
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase("   ")]
+        public void Start_IsNullEmptyOrWhiteSpace_ThrowArgumentException(string containerId)
+        {
+            Assert.That(() => worker.Start(containerId), Throws.Exception.InstanceOf<ArgumentException>());
+        }
+
+        [Test]
+        public void Stop_IsCalledBeforeStop_ThrowInvalidOperationException()
+        {
+            Assert.That(() => worker.Stop(), Throws.Exception.InstanceOf<InvalidOperationException>());
+        }
+    }
+}
