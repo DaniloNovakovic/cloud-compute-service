de8aaf6e2cdda58963d67d1c87a672824789c3a0
Danilo Novakovic <dakenzi97@gmail.com>
2019-04-21 14:53:38 +0200

    Moved assembly loading communication with Containers into seperate static ContainerController class

---
+++ Compute/ContainerController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Common;
+
+namespace Compute
+{
+    internal static class ContainerController
+    {
+        public static Task SendLoadSignalToContainers(List<AssemblyInfo> assemblies)
+        {
+            var taskList = new List<Task>();
+            foreach (var assembly in assemblies)
+            {
+                taskList.Add(Task.Factory.StartNew(
+                    (dynamic dobj) =>
+                    {
+                        ContainerController.SendLoadSignalToContainer(dobj.port, dobj.assemblyPath);
+                    },
+                    new { port = assembly.Port, assemblyPath = assembly.AssemblyFullPath }));
+            }
+
+            return Task.WhenAll(taskList);
+        }
+
+        public static void SendLoadSignalToContainer(int port, string assemblyPath)
+        {
+            Thread.Sleep(1000);
+
+            string remoteAddress = $"net.tcp://localhost:{port}/{typeof(IContainerManagement).Name}";
+            while (true)
+            {
+                try
+                {
+                    var channelFactory = new ChannelFactory<IContainerManagement>(new NetTcpBinding(), remoteAddress);
+                    var proxy = channelFactory.CreateChannel();
+                    string result = proxy.Load(assemblyPath);
+                    Console.WriteLine(result);
+                    channelFactory.Close();
+                    return;
+                }
+                catch (Exception ex)
+                {
+                    Console.Error.WriteLine(ex.Message);
+                }
+
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
--- Compute/Program.cs
+++ Compute/Program.cs
@@ -3,10 +3,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.ServiceModel;
-using System.Threading;
-using System.Threading.Tasks;
-using Common;
 
 namespace Compute
 {
@@ -23,9 +19,7 @@ namespace Compute
             {
                 var package = StartPeriodicCheckUntilFirstValidPackageIsFound(configItem);
                 var destinationPaths = CopyAssemblies(configItem, processManager, package);
-
-                Console.WriteLine("Sending load assembly signal to requested number of container processes...");
-                LoadAssemblies(destinationPaths).GetAwaiter().GetResult();
+                SendLoadAssemblySignalToContainers(destinationPaths);
 
                 Console.WriteLine("All of the processes have finished loading assemblies");
             }
@@ -40,6 +34,13 @@ namespace Compute
             processManager.StopAllProcesses();
         }
 
+        private static void SendLoadAssemblySignalToContainers(List<AssemblyInfo> destinationPaths)
+        {
+            Console.WriteLine("Sending load assembly signal to requested number of container processes...");
+
+            ContainerController.SendLoadSignalToContainers(destinationPaths).GetAwaiter().GetResult();
+        }
+
         private static List<AssemblyInfo> CopyAssemblies(ComputeConfigurationItem configItem, ProcessManager processManager, PackageReaderResult package)
         {
             Console.WriteLine($"Copying assemblies to n={package.NumberOfInstances} destinations...");
@@ -47,24 +48,6 @@ namespace Compute
             var ports = processManager.GetAllContainerPorts().Take(package.NumberOfInstances ?? 0).ToList();
             string sourceDllFullPath = Path.Combine(configItem.PackageFullFolderPath, package.AssemblyName);
             return packageManager.CopyAssemblies(sourceDllFullPath, configItem.PackageFullFolderPath, ports);
-        }
-
-        private static PackageReaderResult StartPeriodicCheckUntilFirstValidPackageIsFound(ComputeConfigurationItem configItem)
-        {
-            Console.WriteLine("Starting periodic check until first valid package is found...");
-
-            var package = packageManager.PeriodicallyCheckForValidPackage(configItem);
-            Debug.WriteLine(package);
-            return package;
-        }
-
-        private static ProcessManager StartUpContainerProcesses(ComputeConfigurationItem configItem)
-        {
-            Console.WriteLine("Starting up container processes...");
-
-            var processManager = ProcessManager.Instance;
-            processManager.StartContainerProcesses(configItem);
-            return processManager;
         }
 
         private static ComputeConfigurationItem LoadComputeConfiguration()
@@ -76,45 +59,22 @@ namespace Compute
             return configItem;
         }
 
-        private static Task LoadAssemblies(List<AssemblyInfo> assemblies)
+        private static ProcessManager StartUpContainerProcesses(ComputeConfigurationItem configItem)
         {
-            var taskList = new List<Task>();
-            foreach (var assembly in assemblies)
-            {
-                taskList.Add(Task.Factory.StartNew(
-                    (dynamic dobj) =>
-                    {
-                        SendLoadSignalToContainers(dobj.port, dobj.assemblyPath);
-                    },
-                    new { port = assembly.Port, assemblyPath = assembly.AssemblyFullPath }));
-            }
+            Console.WriteLine("Starting up container processes...");
 
-            return Task.WhenAll(taskList);
+            var processManager = ProcessManager.Instance;
+            processManager.StartContainerProcesses(configItem);
+            return processManager;
         }
 
-        private static void SendLoadSignalToContainers(int port, string assemblyPath)
+        private static PackageReaderResult StartPeriodicCheckUntilFirstValidPackageIsFound(ComputeConfigurationItem configItem)
         {
-            Thread.Sleep(1000);
+            Console.WriteLine("Starting periodic check until first valid package is found...");
 
-            string remoteAddress = $"net.tcp://localhost:{port}/{typeof(IContainerManagement).Name}";
-            while (true)
-            {
-                try
-                {
-                    var channelFactory = new ChannelFactory<IContainerManagement>(new NetTcpBinding(), remoteAddress);
-                    var proxy = channelFactory.CreateChannel();
-                    string result = proxy.Load(assemblyPath);
-                    Console.WriteLine(result);
-                    channelFactory.Close();
-                    return;
-                }
-                catch (Exception ex)
-                {
-                    Console.Error.WriteLine(ex.Message);
-                }
-
-                Thread.Sleep(1000);
-            }
+            var package = packageManager.PeriodicallyCheckForValidPackage(configItem);
+            Debug.WriteLine(package);
+            return package;
         }
     }
 }
