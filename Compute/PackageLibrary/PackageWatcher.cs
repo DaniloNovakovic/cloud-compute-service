149a769f8bb142de26331a7e0439c47ff12e4f7d
Danilo Novakovic <dakenzi97@gmail.com>
2019-04-26 19:20:02 +0200

    Refactored PackageWatcher class to make it more readable

--- Compute/PackageLibrary/PackageWatcher.cs
+++ Compute/PackageLibrary/PackageWatcher.cs
@@ -9,23 +9,17 @@ namespace Compute
 {
     internal class PackageWatcher
     {
+        private ComputeConfigurationItem configItem;
+
         public void Start(ComputeConfigurationItem configItem)
         {
-            var package = this.PeriodicallyCheckForValidPackageAsync(configItem).GetAwaiter().GetResult();
-            OnValidPackageFound(configItem, package);
-        }
-
-        private static void OnValidPackageFound(ComputeConfigurationItem configItem, PackageReaderResult package)
-        {
-            var ports = ProcessManager.Instance.GetAllContainerPorts().Take(package.NumberOfInstances ?? 0).ToList();
-            var destinationAssemblies = CopyAssemblies(configItem, package, ports);
-            ContainerController.SendLoadSignalToContainersAsync(destinationAssemblies).GetAwaiter().GetResult();
-        }
-
-        private static IEnumerable<PackageAssemblyInfo> CopyAssemblies(ComputeConfigurationItem configItem, PackageReaderResult package, List<ushort> ports)
-        {
-            string sourceDllFullPath = Path.Combine(configItem.PackageFullFolderPath, package.AssemblyName);
-            return new PackageController().CopyAssemblies(sourceDllFullPath, configItem.PackageFullFolderPath, ports);
+            this.configItem = configItem;
+            var package = this.AttemptToReadValidPackage();
+            if (package != null)
+            {
+                this.OnValidPackageFound(package);
+            }
+            this.StartWatchingPackageFolder();
         }
 
         /// <summary>
@@ -33,31 +27,51 @@ namespace Compute
         /// </summary>
         /// <exception cref="ArgumentException"></exception>
         /// <exception cref="ArgumentNullException"></exception>
-        public async Task<PackageReaderResult> PeriodicallyCheckForValidPackageAsync(ComputeConfigurationItem configItem)
+        private PackageReaderResult AttemptToReadValidPackage()
         {
-            var packageController = new PackageController();
-            string packageConfigPath = Path.Combine(configItem.PackageFullFolderPath, configItem.PackageConfigFileName);
-            while (true)
-            {
-                try
-                {
-                    return packageController.ReadPackage(packageConfigPath, maxAllowedNumberOfInstances: configItem.NumberOfContainersToStart);
-                }
-                catch (ConfigurationException configEx)
-                {
-                    Console.Error.WriteLine($"ConfigurationException occured while trying to read {packageConfigPath}. Reason: " + configEx.Message);
-                    if (packageController.DeletePackage(configItem.PackageFullFolderPath))
-                    {
-                        Console.WriteLine($"Successfully deleted {configItem.PackageFullFolderPath}");
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Console.Error.WriteLine("Exception occured while trying to read package. Reason: " + ex.Message);
-                }
+            string packageConfigPath = Path.Combine(this.configItem.PackageFullFolderPath, this.configItem.PackageConfigFileName);
 
-                await Task.Delay(configItem.PackageAcquisitionIntervalMilliseconds).ConfigureAwait(false);
+            try
+            {
+                return new PackageController().ReadPackage(
+                    packageConfigPath,
+                    maxAllowedNumberOfInstances: this.configItem.NumberOfContainersToStart);
             }
+            catch (ConfigurationException configEx)
+            {
+                OnPackageConfigurationError(packageConfigPath, configEx);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine("Exception occured while trying to read package. Reason: " + ex.Message);
+            }
+            return null;
+        }
+
+        private IEnumerable<PackageAssemblyInfo> CopyAssemblies(PackageReaderResult package, List<ushort> ports)
+        {
+            string sourceDllFullPath = Path.Combine(this.configItem.PackageFullFolderPath, package.AssemblyName);
+            return new PackageController().CopyAssemblies(sourceDllFullPath, this.configItem.PackageFullFolderPath, ports);
+        }
+
+        private void OnPackageConfigurationError(string packageConfigPath, ConfigurationException configEx)
+        {
+            Console.Error.WriteLine($"ConfigurationException occured while trying to read {packageConfigPath}. Reason: " + configEx.Message);
+            if (new PackageController().DeletePackage(this.configItem.PackageFullFolderPath))
+            {
+                Console.WriteLine($"Successfully deleted {this.configItem.PackageFullFolderPath}");
+            }
+        }
+
+        private void OnValidPackageFound(PackageReaderResult package)
+        {
+            var ports = ProcessManager.Instance.GetAllContainerPorts().Take(package.NumberOfInstances ?? 0).ToList();
+            var destinationAssemblies = this.CopyAssemblies(package, ports);
+            ContainerController.SendLoadSignalToContainersAsync(destinationAssemblies).GetAwaiter().GetResult();
+        }
+
+        private void StartWatchingPackageFolder()
+        {
         }
     }
 }
