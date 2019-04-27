a465e3c03887cd08a3c3b883d9c2817115f69eb5
Danilo Novakovic <dakenzi97@gmail.com>
2019-04-27 21:35:12 +0200

    Created Role and RoleInstance helper classes

---
+++ Compute/Role.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compute
+{
+    internal class Role
+    {
+        public string Name { get; }
+        public IEnumerable<RoleInstance> Instances { get; }
+
+        public Role(string name)
+        {
+            this.Name = name;
+            Instances = new List<RoleInstance>();
+        }
+    }
+}
---
+++ Compute/RoleInstance.cs
@@ -0,0 +1,9 @@
+﻿namespace Compute
+{
+    internal class RoleInstance
+    {
+        public ushort ContainerId { get; set; }
+        public string AssemblyFullPath { get; set; }
+        public Role Role { get; set; }
+    }
+}
