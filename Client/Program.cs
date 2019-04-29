6653a4934f682b4e5916d4f56cc1c2af0a78d82f
Danilo Novakovic <dakenzi97@gmail.com>
2019-04-29 05:50:15 +0200

    Added wcf client as console application that can change number of instances for a role

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,15 +1,52 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Configuration;
+using System.ServiceModel;
+using Common;
 
 namespace Client
 {
     internal static class Program
     {
-        private static void Main(string[] args)
+        private const string assemblyConfigKeyName = "assemblyName";
+
+        private static void Main()
         {
+            string assemblyName = ConfigurationManager.AppSettings.Get(assemblyConfigKeyName)
+                ?? throw new ConfigurationErrorsException($"<appSettings> <add key='{assemblyConfigKeyName}' value='...' /> in configuration is missing");
+
+            Console.WriteLine($"{nameof(assemblyName)}: {assemblyName}");
+
+            while (true)
+            {
+                Console.Write("Enter scale value (integer): ");
+
+                if (!int.TryParse(Console.ReadLine(), out int scaleCount))
+                {
+                    Console.Error.WriteLine("Failed to parse entered number. Please try again...");
+                    continue;
+                }
+
+                SafeScale(assemblyName, scaleCount);
+            }
+        }
+
+        private static void SafeScale(string assemblyName, int scaleCount)
+        {
+            try
+            {
+                var factory = new ChannelFactory<IComputeManagement>(
+                    endpointConfigurationName: typeof(IComputeManagement).FullName);
+                var proxy = factory.CreateChannel();
+
+                string response = proxy.Scale(assemblyName, scaleCount);
+                Console.WriteLine(response);
+
+                factory.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine(ex);
+            }
         }
     }
 }
